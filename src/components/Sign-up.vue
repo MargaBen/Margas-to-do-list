af817bc9b09b231f396b629df6fadd3d557909ca
Marga <margarita.benitez.lopez@gmail.com>
2022-10-22 18:24:16 +0200

    Sign Up component

--- src/components/Sign-up.vue
+++ src/components/Sign-up.vue
@@ -15,7 +15,7 @@ const confirmPassword = ref(null);
 const isPwd = ref(true);
 const isConfirmPwd = ref(true);
 
-async function onSubmit() {
+async function goSignUp() {
   if (password.value !== confirmPassword.value) {
     console.log("Passwords not matching");
   } else {
@@ -30,3 +30,76 @@ async function onSubmit() {
   }
 }
 </script>
+
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column">
+      <!-- <q-input v-model="password" filled type="password" hint="Password" /> -->
+      <q-form @submit="goSignUp()" class="q-gutter signup-class" :width="550">
+        <q-input
+          v-model="email"
+          :dense="dense"
+          type="email"
+          label="please enter your e-mail"
+          hint=""
+        />
+        <q-input
+          v-model="password"
+          :dense="dense"
+          :type="isPwd ? 'password' : 'text'"
+          hint=""
+          label="create password"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+
+        <q-input
+          v-model="confirmPassword"
+          :dense="dense"
+          :type="isPwd ? 'password' : 'text'"
+          label="confirm password"
+          hint=""
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
+      <!-- <q-input v-model="search" filled type="search" hint="Search">
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input> -->
+
+      <!-- <q-input v-model="tel" filled type="tel" hint="Telephone number" />
+
+      <q-input v-model="url" filled type="url" hint="URL" />
+
+      <q-input v-model="time" filled type="time" hint="Native time" />
+
+      <q-input v-model="date" filled type="date" hint="Native date" /> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.signup-class {
+  display: flex;
+  flex-direction: column;
+  width: 55%;
+  margin: 0 22.5% 0;
+}
+</style>
--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,5 +1,8 @@
-<script setup></script>
+<script setup>
+import Signup from "src/components/Sign-up.vue";
+</script>
 
 <template>
   <h1>ToDo List</h1>
+  <Signup />
 </template>
